fa11c7c323bf8e89c818bdc4dea895410b95bafd
andre.scartezini <andre.scartezini@druid.com.br>
2021-10-20 17:35:40 -0300

    adding comments

--- main.go
+++ main.go
@@ -20,10 +20,15 @@ func (fs FileSystem) Ls(path string) []string {
 	paths := strings.Split(path, "/")
 	fileNode := fs.Root
 
-	for i := 1; i < len(paths); i++ {
-		fileNode = fileNode.Children[paths[i]]
+	if path != "/" {
+		// loops over the sub paths till it reaches the last one
+		for i := 1; i < len(paths); i++ {
+			// sets the current filenode as root
+			fileNode = fileNode.Children[paths[i]]
+		}
 	}
 
+	// reads all children filenodes of the last sub path
 	keys := []string{}
 	for k, v := range fileNode.Children {
 		keys = append(keys, k)
@@ -37,11 +42,12 @@ func doNothing(v interface{}) {}
 
 func (fs FileSystem) MkDir(path string) {
 	paths := strings.Split(path, "/")
-	fileNode := fs.Root
+	fileNode := fs.Root // file node starts with root
 
 	for i := 1; i < len(paths); i++ {
 		v := paths[i]
 
+		// create a new entry in the map if the path does not exist
 		if _, exists := fileNode.Children[v]; !exists {
 			if fileNode.Children == nil {
 				fileNode.Children = map[string]*File{}
@@ -50,13 +56,18 @@ func (fs FileSystem) MkDir(path string) {
 			fileNode.Children[v] = &File{IsDir: true}
 		}
 
+		// sets the current filenode as root
 		fileNode = fileNode.Children[v]
 	}
 }
 
 func main() {
 	fs := FileSystem{&File{IsDir: true, Name: "/"}}
-	fs.MkDir("/apps/golang/gorocks")
-	fs.MkDir("/apps/golang/go-concurrency")
-	fmt.Println(fs.Ls("/apps/golang"))
+	fs.MkDir("/apps/golang/pointers")
+	fs.MkDir("/apps/golang/concurrency")
+	fs.MkDir("/apps/nodejs/streams")
+	fs.MkDir("/apps/nodejs/event-loop")
+	fs.MkDir("/docs")
+	fs.MkDir("/images")
+	fmt.Println(fs.Ls("/"))
 }
